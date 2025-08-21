3bab35dabf664c5c076bd8b5924ba4c4ce761a0b
zym <202213093023@cuc.edu.cn>
2025-08-21 17:40:47 +0800

    Add initial Android platform and configuration
    Introduces the Android platform to the project, including Gradle build files, app manifest, resources, icons, splash screens, and Java source files. Updates configuration in capacitor.config.ts and package.json to support Android. This sets up the project for building and running as an Android app using Capacitor.

---
+++ android/app/src/main/java/io/ionic/relieve_sorrow/MainActivity.java
@@ -0,0 +1,5 @@
+package io.ionic.relieve_sorrow;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.relieve_sorrow',
   appName: 'Relieve Sorrow',
   webDir: 'dist'
 };
